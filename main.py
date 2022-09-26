a3e68493edbac30ac1208d34f15d92073fc509b1
ceo-py <sfmf@abv.bg>
2022-09-26 18:42:45 +0300

    zero opening with numbers around, restart button and winner button, game over also

--- main.py
+++ main.py
@@ -2,34 +2,56 @@ import pygame
 from pygame.locals import *
 from minesweeper import *
 
-FPS = 60
+
 pygame.init()
+FPS = 60
 WIDTH = 400
 HEIGHT = 400
+MAIN_FONT = pygame.font.SysFont("comicsans", 50)
+SIZE_R = WIDTH / ROW
+SIZE_C = WIDTH / COL
 pygame.display.set_caption("Minesweeper beta v0.000000000000001")
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
-SIZE = WIDTH / ROW
+mine_field = start_game()
 
 
 def re_scale_all_pictures():
     for key, link in PICTURES.items():
-        PICTURES[key] = pygame.transform.scale(pygame.image.load(link), (SIZE, SIZE))
+        PICTURES[key] = pygame.transform.scale(pygame.image.load(link), (SIZE_C, SIZE_R))
 
 
-def game_over_result():
+def check_for_game_winner():
+    squares_un_open = 0
     for row in range(START_ROW, ROW):
         for col in range(COL):
-            mine_field[row][col].show_square()
+            if not mine_field[row][col].open_field:
+                squares_un_open += 1
+    return squares_un_open
+
+
+def game_over_result(show=True):
+    for row in range(START_ROW, ROW):
+        for col in range(COL):
+            if not mine_field[row][col].open_field:
+                if show:
+                    mine_field[row][col].show_square()
+                mine_field[row][col].open_field = True
 
 
 def draw_square():
-    for row in range(START_ROW, ROW):
+    for row in range(ROW):
         for col in range(COL):
-            window.blit(PICTURES[mine_field[row][col].picture], (col * SIZE, row * SIZE))
+            if row in (0, 1) and col == 4:
+                window.blit(PICTURES[mine_field[row][col].picture], (4 * SIZE_C, 0.5 * SIZE_R))
+            else:
+                window.blit(PICTURES[mine_field[row][col].picture], (col * SIZE_C, row * SIZE_R))
+    score_text = MAIN_FONT.render(str(Figure.flag_counter), 1, ("red"))
+    window.blit(score_text, (20, 0))
 
 
 re_scale_all_pictures()
+
 
 while running:
     pygame.time.Clock().tick(FPS)
@@ -38,9 +60,10 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             left_click, middle_click, right_click = pygame.mouse.get_pressed()
-            col, row = [int(x // SIZE) for x in pygame.mouse.get_pos()]
+            col, row = [int(x // size) for x, size in zip(pygame.mouse.get_pos(), [SIZE_C, SIZE_R])]
             symbol = mine_field[row][col]
-
+            if symbol.name == "Blank":
+                continue
             if right_click:
                 if not symbol.open_field:
                     symbol.change_flag()
@@ -50,12 +73,25 @@ while running:
                     continue
                 if symbol.name == "unclicked_bomb":
                     symbol.name = "clicked_bomb"
+                    mine_field[0][4].picture = "square_death"
+                    mine_field[1][4].picture = "square_death"
                     game_over_result()
+
                 elif symbol.name == 0:
                     open_zero_field(row, col)
 
+                elif symbol.name == "Menu":
+                    Figure.flag_counter = BOMB_NUMBER
+                    mine_field = start_game()
+                    re_scale_all_pictures()
+
                 symbol.show_square()
                 symbol.open_field = True
+
+    if check_for_game_winner() == BOMB_NUMBER:
+        mine_field[0][4].picture = "square_winner"
+        mine_field[1][4].picture = "square_winner"
+        game_over_result(False)
 
     draw_square()
     pygame.display.update()
--- minesweeper.py
+++ minesweeper.py
@@ -1,23 +1,25 @@
 import random
 import os
 
-ROW = 9
+ROW = 11
 COL = 9
-START_ROW = 0
-bombs_number = 10
+START_ROW = 2
+BOMB_NUMBER = 3
 field_to_explore = []
 CURRENT_PATH = os.getcwd()
 PICTURES = {}
-bombs_pos = []
 directions = {
     "up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0),
     "top left diagonal": (-1, -1), "bottom left diagonal": (-1, 1),
     "top right diagonal": (1, -1), "bottom right diagonal": (1, 1)}
-mine_field = [[int(0) for x in range(COL)] for _ in range(ROW)]
+
+
+def create_empty_matrix():
+    return [[int(0) for x in range(COL)] for _ in range(ROW)]
 
 
 def load_pictures():
-    for file_name in os.listdir("Pictures"):
+    for file_name in os.listdir("pictures"):
         if not file_name.endswith(".png"):
             continue
         digit = file_name[-5]
@@ -31,44 +33,64 @@ def check_valid_index(row, col):
     return 0 <= row < ROW and 0 <= col < COL
 
 
-def generate_bombs():
-    while len(bombs_pos) != bombs_number:
+def generate_bombs(mine_field):
+    bombs_pos = []
+    while len(bombs_pos) != BOMB_NUMBER:
         bomb_pos = [random.randrange(START_ROW, ROW), random.randrange(0, COL)]
         if bomb_pos not in bombs_pos:
             bombs_pos.append(bomb_pos)
 
     for row, col in bombs_pos:
         mine_field[row][col] = "*"
+    return mine_field
 
 
-load_pictures()
-generate_bombs()
-
-
-def check_for_bombs_in_range(row, col):
-    current_sum = 0
-    for moving_col, moving_row in directions.values():
-        check_row, check_col = row + moving_row, col + moving_col
-        if check_valid_index(check_row, check_col) and mine_field[check_row][check_col] == "*":
-            current_sum += 1
-    mine_field[row][col] = current_sum
-
-
-for row in range(START_ROW, ROW):
-    for col in range(COL):
-        if mine_field[row][col] != "*":
-            check_for_bombs_in_range(row, col)
+def check_for_bombs_in_range(mine_field):
+    for row in range(ROW):
+        for col in range(COL):
+            if row < START_ROW:
+                continue
+            if mine_field[row][col] != "*":
+                current_sum = 0
+                for moving_col, moving_row in directions.values():
+                    check_row, check_col = row + moving_row, col + moving_col
+                    if check_valid_index(check_row, check_col) and mine_field[check_row][check_col] == "*":
+                        current_sum += 1
+                mine_field[row][col] = current_sum
+    return mine_field
 
 
 def open_zero_field(row, col):
     if check_valid_index(row, col) and mine_field[row][col].name == 0 and mine_field[row][col].visited == "No":
+        for d_row, d_col in directions.values():
+            c_row, c_cow = row + d_row, col + d_col
+            if check_valid_index(c_row, c_cow) and isinstance(mine_field[c_row][c_cow].name, int) \
+                    and mine_field[c_row][c_cow].name > 0:
+                mine_field[c_row][c_cow].picture = mine_field[c_row][c_cow].name
+                mine_field[row][col].visited = "Yes"
+                mine_field[c_row][c_cow].open_field = True
+
         mine_field[row][col].picture = mine_field[row][col].name
         mine_field[row][col].visited = "Yes"
         mine_field[row][col].open_field = True
-        [open_zero_field(row, col) for row, col in ((row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col))]
+        [open_zero_field(row, col) for row, col in
+         ((row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col))]
+
+
+def start_game():
+    global mine_field
+    mine_field = create_empty_matrix()
+    load_pictures()
+    mine_field = generate_bombs(mine_field)
+    mine_field = check_for_bombs_in_range(mine_field)
+    mine_field = create_table(mine_field)
+    return mine_field
 
 
 class Figure:
+    flag_counter = BOMB_NUMBER
+    alive = True
+
     def __init__(self, name):
         self.name = name
         self.visited = "No"
@@ -77,26 +99,35 @@ class Figure:
         self.picture = "square"
 
     def change_flag(self):
+        if self.open_field:
+            return
         if self.got_flag:
+            Figure.flag_counter += 1
             self.got_flag = False
             self.picture = "square"
         else:
+            Figure.flag_counter -= 1
             self.got_flag = True
             self.picture = "flag"
 
     def show_square(self):
         self.picture = self.name
 
-    def clicked_field(self):
-        self.open_field = True
 
+def create_table(mine_field):
+    for row in range(ROW):
+        for col in range(COL):
+            if row in (0, 1) and col == 4:
+                mine_field[row][col] = Figure("Menu")
+                mine_field[row][col].picture = "square_restart"
 
-for row in range(START_ROW, ROW):
-    for col in range(COL):
-        if mine_field[row][col] != "*":
-            symbol = mine_field[row][col]
-            mine_field[row][col] = Figure(symbol)
-        else:
-            mine_field[row][col] = Figure("unclicked_bomb")
+            elif row < START_ROW:
+                mine_field[row][col] = Figure("Blank")
+                mine_field[row][col].picture = "square_b"
 
-# [print(*mine_field[row]) for row in range(ROW)]
+            elif mine_field[row][col] != "*":
+                symbol = mine_field[row][col]
+                mine_field[row][col] = Figure(symbol)
+            else:
+                mine_field[row][col] = Figure("unclicked_bomb")
+    return mine_field
